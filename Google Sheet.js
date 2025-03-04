026f0ab70ca80980f3184d15f1eca91f48f95bfa
Xako Sun <xaviemiello@gmail.com>
2025-03-04 14:39:15 +0100

    Update Google Sheet.js

--- Google Sheet.js
+++ Google Sheet.js
@@ -1,4 +1,4 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzpxWwfaLZgA2go8ijvQjL1iFH9-laoYpqiEZn1L7ai8fpdMvWutlwC4XcRvLDkf3bsOg/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwxrr9gY3hHl86SuG0uv8nQa0NJHnmqFYqzCzrqM0w4wWDuE-B--j9r9iwch5FHZK6o/exec'
 
 const form = document.forms['contact-form']
 
